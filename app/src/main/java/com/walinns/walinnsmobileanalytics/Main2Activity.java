abea0dc1f1a8a909ff1ccb0d236fae47cd2c7f57
PrettyAndroid <rejoylin2015@gmail.com>
2018-06-22 10:49:28 +0530

    1.0.1 published to maven

--- app/src/main/java/com/walinns/walinnsmobileanalytics/Main2Activity.java
+++ app/src/main/java/com/walinns/walinnsmobileanalytics/Main2Activity.java
@@ -1,52 +1,31 @@
 package com.walinns.walinnsmobileanalytics;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 
-import android.support.v4.content.ContextCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import android.view.MotionEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+
 
 import com.walinns.walinnsapi.WalinnsAPI;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
-
 
 public class Main2Activity extends AppCompatActivity {
-    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
-    WebView webView;
-    private float m_downX;
-    private String postUrl = "https://jsonformatter.curiousconcept.com/";
 
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);//b2bac52c84ea0f0a4139fbaecf99936e
         WalinnsAPI.getInstance().initialize(Main2Activity.this,"3c149e37c460d419a946");
-        webView = (WebView) findViewById(R.id.webView);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,183 +33,9 @@ public class Main2Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-       // getPermissionToReadUserContacts();
-
-        System.out.println("MainActivity ip address:" + getMobileIPAddress() );
-//        WalinnsAPI.getInstance().track("ScreenName");
-//
-//        Intent i = new Intent("com.android.vending.INSTALL_REFERRER");
-//        i.putExtra("referrer","utm_source=linkedin&utm_medium=banner&utm_campaign=newinstall");
-////Set Package name
-//        i.setPackage("com.walinns.walinnsmobileanalytics");
-////referrer is a composition of the parameter of the campaing
-//
-//        sendBroadcast(i);
-       // initWebView();
-       //  renderPost();
-
-        ActivityInfo activityInfo = null;
-        try {
-            activityInfo = getPackageManager().getActivityInfo(
-                    getComponentName(), PackageManager.GET_META_DATA);
-            String title = activityInfo.loadLabel(getPackageManager())
-                    .toString();
-            System.out.println("Activity name :" + title);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void getPermissionToReadUserContacts() {
-        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
-        // checking the build version since Context.checkSelfPermission(...) is only available
-        // in Marshmallow
-        // 2) Always check for permission (even if permission has already been granted)
-        // since the user can revoke permissions at any time through Settings
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // The permission is NOT already granted.
-            // Check if the user has been asked about this permission already and denied
-            // it. If so, we want to give more explanation about why the permission is needed.
-            if (shouldShowRequestPermissionRationale(
-                    android.Manifest.permission.READ_CONTACTS)) {
-                // Show our own UI to explain to the user why we need to read the contacts
-                // before actually requesting the permission and showing the default UI
-            }
-
-            // Fire off an async request to actually get the permission
-            // This will show the standard permission request dialog UI
-            requestPermissions(new String[]{android.Manifest.permission.READ_CONTACTS},
-                    READ_CONTACTS_PERMISSIONS_REQUEST);
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        // Make sure it's our original READ_CONTACTS request
-        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
-            if (grantResults.length == 1 &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-    public static String getMobileIPAddress() {
-        try {
-            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface intf : interfaces) {
-                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
-                for (InetAddress addr : addrs) {
-                    if (!addr.isLoopbackAddress()) {
-                        return  addr.getHostAddress();
-                    }
-                }
-            }
-        } catch (Exception ex) { } // for now eat exceptions
-        return "";
-    }
-    private void initWebView() {
-        webView.setWebChromeClient(new MyWebChromeClient(this));
-        webView.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                /**
-                 * Check for the url, if the url is from same domain
-                 * open the url in the same activity as new intent
-                 * else pass the url to browser activity
-                 * */
-                if (isSameDomain(postUrl, url)) {
-                    Intent intent = new Intent(Main2Activity.this, Main2Activity.class);
-                    intent.putExtra("postUrl", url);
-                    startActivity(intent);
-                } else {
-                    // launch in-app browser i.e BrowserActivity
-                  //  openInAppBrowser(url);
-                }
-
-                return true;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-             }
-        });
-        webView.clearCache(true);
-        webView.clearHistory();
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setHorizontalScrollBarEnabled(false);
-        webView.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getPointerCount() > 1) {
-                    //Multi touch detected
-                    return true;
-                }
-
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: {
-                        // save the x
-                        m_downX = event.getX();
-                    }
-                    break;
-
-                    case MotionEvent.ACTION_MOVE:
-                    case MotionEvent.ACTION_CANCEL:
-                    case MotionEvent.ACTION_UP: {
-                        // set x so that it doesn't move
-                        event.setLocation(m_downX, event.getY());
-                    }
-                    break;
-
-                }
-
-                return false;
-            }
-        });
-    }
-
-    private void renderPost() {
-        webView.loadUrl(postUrl);
-
-        // webView.loadUrl("file:///android_asset/sample.html");
-    }
-    private class MyWebChromeClient extends WebChromeClient {
-        Context context;
-
-        public MyWebChromeClient(Context context) {
-            super();
-            this.context = context;
-        }
 
 
-    }
-    public static boolean isSameDomain(String url, String url1) {
-        return getRootDomainUrl(url.toLowerCase()).equals(getRootDomainUrl(url1.toLowerCase()));
-    }
-
-    private static String getRootDomainUrl(String url) {
-        String[] domainKeys = url.split("/")[2].split("\\.");
-        int length = domainKeys.length;
-        int dummy = domainKeys[0].equals("www") ? 1 : 0;
-        if (length - dummy == 2)
-            return domainKeys[length - 2] + "." + domainKeys[length - 1];
-        else {
-            if (domainKeys[length - 1].length() == 2) {
-                return domainKeys[length - 3] + "." + domainKeys[length - 2] + "." + domainKeys[length - 1];
-            } else {
-                return domainKeys[length - 2] + "." + domainKeys[length - 1];
-            }
-        }
-    }
 }
